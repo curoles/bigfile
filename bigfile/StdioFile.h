0a89d8c8174eca9476b8fbfc1a68209a9fbc7433
curoles <curoles@yahoo.com>
2018-10-11 00:21:37 -0700

    Add file::stdio::Instance.readAsString()

--- bigfile/StdioFile.h
+++ bigfile/StdioFile.h
@@ -8,17 +8,41 @@
 #pragma once
 
 #include <string>
+#include <tuple>
 #include <cstdio>
+#include <cerrno>
+#include <cstring>
 
 namespace file::stdio {
+
+static inline
+void read_all_with_fgetc(FILE* fp, std::string& s)
+{
+    int c; while ((c = ::fgetc(fp)) != EOF) { s += (char)c; }
+}
+
+static inline
+void read_all_with_fread(FILE* fp, std::string& s)
+{
+    char buf[1024]; size_t num_bytes_received;
+    while ((num_bytes_received = ::fread(buf, 1, sizeof(buf), fp)) != 0) {
+        s.append(buf, num_bytes_received);
+    }
+}
 
 class Instance
 {
     FILE* file_ = nullptr;
 public:
+    Instance():file_{nullptr}{}
    ~Instance() {
         close();
     }
+    Instance(Instance&&) = default;
+    Instance(const Instance&) = delete;
+
+    Instance& operator=(Instance&&) = delete;
+    Instance& operator=(const Instance&) = delete;
 
     bool isOpen() const {
         return file_ != nullptr;
@@ -40,6 +64,27 @@ public:
     bool write(const std::string& str) {
         int ret = ::fputs(str.c_str(), file_);
         return ret > 0;
+    }
+
+    void setAtBeginning() {
+        ::fseek(file_, 0, SEEK_SET);
+    }
+
+    std::tuple<std::string,bool,std::string>
+    readAsString(bool fromBeginning=true, void (*reader)(FILE*,std::string&) = read_all_with_fread)
+    {
+        std::string s, errmsg; bool err = false;
+
+        if (fromBeginning) setAtBeginning();
+        reader(file_,s);
+
+        if (::ferror(file_)) {
+            err = true; errmsg = ::strerror(::ferror(file_));
+        } else if (! ::feof(file_)) {
+            err = true; errmsg = "End of file NOT reached";
+        }
+
+        return std::make_tuple(s, err, errmsg);
     }
 };
 
--- test/TestStdioFile.cpp
+++ test/TestStdioFile.cpp
@@ -1,13 +1,39 @@
 #include "StdioFile.h"
-//#include "SmallFile.h"
+
+#include <cassert>
+#include <iostream>
 
 int main()
 {
-    auto f = file::stdio::open("test_stdio.txt", "w");
-    f.write("word1 word2\n word3");
+    std::string fname = "test_stdio.txt";
 
-    //auto sf = file::small::open("test_small.txt", "w");
-    //sf.write("w1\nword2\nword3");
+    std::string written_str = "word1\n word2 word3\nword4\n";
 
-    return 0;
+    {
+    auto f = file::stdio::open(fname, "w");
+    f.write(written_str);
+    f.close();
+    assert(!f.isOpen());
+    }
+
+    {
+    auto f2 = file::stdio::open(fname, "r");
+    assert(f2.isOpen());
+    auto [received_str,read_err,read_errmsg] = f2.readAsString();
+    if (read_err) {
+        std::cout << "read error:" << read_errmsg << std::endl;
+        return EXIT_FAILURE;
+    }
+    assert(0 == received_str.compare(written_str) && "shall read what was written");
+    auto [received_str2,read_err2,read_errmsg2] = f2.readAsString(true,file::stdio::read_all_with_fgetc);
+    assert(0 == received_str2.compare(written_str) && "shall read what was written");
+    }
+
+    {
+    auto f = file::stdio::open(fname, "w"); // open for write only
+    auto [received_str,read_err,read_errmsg] = f.readAsString();
+    assert(read_err && "shall not be able to read file opened for writing only");
+    }
+
+    return EXIT_SUCCESS;
 }
