def969a98b6a5d8979a10237d8c3de25960093d5
curoles <curoles@yahoo.com>
2018-10-11 20:53:49 -0700

    Check we read what was written in TestStdioFile

---
+++ bigfile/FileSystem.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief
+ * @author    Igor Lesik 2018
+ * @copyright Igor Lesik 2018
+ *
+ */
+#pragma once
+
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem;
+
